38e640ad5ce34855d006d9a4ada812b8108b0ef2
NoFunny <chunikhin_1999@mail.ru>
2019-10-03 16:04:53 +0700

    Fix_index.js

--- week 3/index.js
+++ week 3/index.js
@@ -3,10 +3,23 @@
  * @returns {Object}
  */
 module.exports = function (date) {
+    // console.log(date);
     var objTime = {
+        _value: null,
+        get value() {
+            // console.log(this._value);
+          return this+'';
+        },
         init: function (date) {
-            this.newDate = new Date(date);
-            return this;
+            if(date instanceof Date) {
+                this._value = date;
+                // console.log(this._value);
+            }else {
+                var arr = date.split(/[\s:-]/);
+                this._value = new Date(arr[0], arr[1] - 1, arr[2], arr[3], arr[4]);
+                // console.log(this._value);
+            }
+                return this;
         },
 
         method: {
@@ -18,33 +31,47 @@ module.exports = function (date) {
         },
 
         add: function (value, method) {
-            // if (value < 0) {
-            //     throw new Error('TypeError ' + value + ' < 0' );
-            /*}else*/ if (method in this.method) {
+            if (value > 0 && method in this.method) {
                 return this.setValue(value, this.method[method]);
             } else {
-                throw new Error('TypeError' + method);;
+                throw new TypeError('TypeError');
             }
-            console.log(this.newDate);
-            // return this;
         },
-        setValue: function(value, method) {
-            console.log(this.newDate);
-            this.newDate["setUTC" + method](value + this.newDate["getUTC" + method]());
+        setValue: function (value, method) {
+            // console.log(this);
+            this._value['set' + method](value + this._value['get' + method]());
+            // console.log(this._value);
             return this;
         },
-        subtract: function(value, method) {
-            console.log(this.newDate);
-            return this.add(value, method);
+
+        setValueSubtract: function (value, method) {
+            this._value['setUTC' + method](-value + this._value['getUTC' + method]());
+            // console.log(date);
+            return this;
         },
-        toString: function() {
-            console.log(this.newDate);
-            return this.newDate.toLocaleString("ru",{timeZone : "UTC"})
+        subtract: function (value, method) {
+            if (value > 0 && method in this.method) {
+                return this.setValueSubtract(value, this.method[method]);
+            } else {
+                throw new TypeError('TypeError');
+            }
         },
-        value: function() {
-            console.log(this.newDate);
-            return this.newDate
-        },
-    }
+        valueOf: function () {
+            var year = this._value.getFullYear();
+            var month = this._value.toLocaleString('ru',{timeZone : 'UTC', month: '2-digit'});
+            var day = this._value.toLocaleString('ru',{timeZone : 'UTC', day: '2-digit'});
+            var time = this._value.toLocaleString("ru",{ hour: '2-digit', minute: '2-digit' });
+            // console.log(time);
+            var dates = year + '-' + month + '-' + day + ' ' + time;
+
+            return dates;
+
+            },
+        toString: function () {
+            // console.log(date);
+            return this.valueOf;
+        }
+    };
+    // console.log(date);
     return objTime.init(date);
 };
